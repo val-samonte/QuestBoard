364e40da2a2f8f30cf27ad98f27aee050cc1609a
Val <val.samonte@gmail.com>
2024-06-24 04:40:50 +0800

    Initial deployment

---
+++ src/components/WalletHandler.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { useMemo } from 'react'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { clusterApiUrl } from '@solana/web3.js'
+
+require('@solana/wallet-adapter-react-ui/styles.css')
+
+export default function WalletHandler({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const network = WalletAdapterNetwork.Devnet
+  const endpoint = useMemo(() => clusterApiUrl(network), [network])
+
+  const wallets = useMemo(
+    () => [],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [network]
+  )
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
+}
